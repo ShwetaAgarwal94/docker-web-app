2c5f0c84b7f02fd1bec92496c3079ee8f2c6f906
Shweta123 <55076914+ShwetaAgarwal94@users.noreply.github.com>
2021-07-01 00:30:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,10 +4,9 @@ print("Content-type:text/html")
 print()
 
 import cgi,subprocess
-print("hellp")
 
-f = cgi.FieldStorage()
-cmd = f.getvalue("x")
+a = cgi.FieldStorage()
+cmd = a.getvalue("s")
 print(cmd)
 output = subprocess.getoutput("sudo " +cmd)
 print(output)
